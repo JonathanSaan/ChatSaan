01ba4424387b9591b833d0bf6969b68f764474d5
JonathanSaan <crosspool03@gmail.com>
2022-08-17 19:14:34 -0300

    up

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,7 @@
 import React, { useEffect } from "react";
 import { Header } from "./components/Header";
-import APIKey from "./data/api";
 
 export const App = () => {
-  
-  useEffect(() => {
-    const load = async () => {
-      const respost = await axios.get(`https://api.rawg.io/api/platforms?key=${APIKey}`);
-    }
-  })
   
   return (
     <>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import "./style.scss";
+
+export const Header = () => {
+  
+  return (
+    <header>
+      <nav>
+        <a href="https://github.com/JonathanSaan?tab=repositories">JonathanSaan</a>
+        <button>
+          into to google
+        </button>
+      </nav>
+    </header>
+  );
+};
