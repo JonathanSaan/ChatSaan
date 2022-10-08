8b8fd80256a1dffa83246e5aef78851c3a7a86d1
JonathanSaan <crosspool03@gmail.com>
2022-10-08 11:46:21 -0300

    up

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_Firebase_API_Key}`,
-	authDomain: `${process.env.REACT_APP_Auth_Domain}`,
-	projectId: `${process.env.REACT_APP_Project_Id}`,
-	storageBucket: `${process.env.REACT_APP_Storage_Bucket}`,
-	messagingSenderId: `${process.env.REACT_APP_Message_Sender_Id}`,
-	appId: `${process.env.REACT_APP_App_Id}`,
-	measurementId: `${process.env.REACT_APP_Measurement_Id}`
+  authDomain: `${process.env.REACT_APP_Auth_Domain}`,
+  projectId: `${process.env.REACT_APP_Project_Id}`,
+  storageBucket: `${process.env.REACT_APP_Storage_Bucket}`,
+  messagingSenderId: `${process.env.REACT_APP_Message_Sender_Id}`,
+  appId: `${process.env.REACT_APP_App_Id}`,
+  measurementId: `${process.env.REACT_APP_Measurement_Id}`
 };
 
 export const app = initializeApp(firebaseConfig);
